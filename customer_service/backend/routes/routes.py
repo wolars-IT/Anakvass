30a6da770757939e1512fee46fca56e275818290
Flop <uyweywgs@gmail.com>
2023-07-12 20:43:43 +0300

    Fixed typing & validation issues

--- customer_service/backend/routes/routes.py
+++ customer_service/backend/routes/routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from schemas.order import Order
+from typing_extensions import TypedDict
 
 
 router = APIRouter()
 
 
 @router.post("/")
-async def create_order(order: Order) -> dict[str, int]:
+async def create_order(order: Order) -> TypedDict("order_id", {"id": int}):
     pass
--- customer_service/backend/schemas/order.py
+++ customer_service/backend/schemas/order.py
@@ -17,14 +17,12 @@ class Statuses(Enum):
 class Order(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    id: int | None
     full_name: constr(min_length=8, max_length=255)
     email: constr(min_length=8, max_length=320)
     phone_number: constr(min_length=6, max_length=15)
     address: constr(min_length=12, max_length=425)
     box_count: conint(gt=0, lt=(2**32))
     comment: constr(min_length=1, max_length=255)
-    status: Statuses | None
 
     @field_validator("full_name")
     def full_name_validator(cls, value):
@@ -52,10 +50,11 @@ class Order(BaseModel):
     @field_validator("address")
     def address_validator(cls, value):
         value = value.strip()
-        if value.count(", ") < 4:
-            raise ValueError("Wrong address format: Address must contain at least 4 separators (\", \")")
+        if value.count(", ") != 4:
+            raise ValueError("Wrong address format: Address must contain 4 separators (\", \")")
         if not value.split(", ")[-1].isdigit():
             raise ValueError("Wrong address format: Wrong zip code: zip code must be a number")
+        return value
 
     @field_validator("comment")
     def comment_validator(cls, value):
