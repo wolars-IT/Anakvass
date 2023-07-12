ac853bccae4400b31f8f41b7ddb26d2c69a1ca24
Danulo Mamchur <danielo83mail@gmail.com>
2023-07-12 21:28:54 +0300

    Minor improvements

--- customer_service/telegram_bot/bot/bot.py
+++ customer_service/telegram_bot/bot/bot.py
@@ -7,12 +7,12 @@ REQUEST = f"https://api.telegram.org/bot{environ.get('TG_BOT_API_KEY', TEST_API_
 
 
 async def send_notification(order):
-    order.update({"time": datetime.now()})
-    message = MESSAGE_TEMPLATE.format(**order)
+    current_time = str(datetime.now().replace(microsecond=0))
+    message = MESSAGE_TEMPLATE.format(**order, time=current_time)
     payload = {
         "chat_id": CHAT_ID,
         "text": message,
-        "parse_mode": "Markdown"
+        "parse_mode": "HTML"
     }
 
     async with aiohttp.ClientSession() as session:
--- customer_service/telegram_bot/routes/routes.py
+++ customer_service/telegram_bot/routes/routes.py
@@ -1,11 +1,21 @@
-from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from fastapi import APIRouter
 from bot.bot import send_notification
+from pydantic import BaseModel
 
 router = APIRouter()
 
 
-# order: {full_name, email, phone_number, address, box_count, comment}
+class Order(BaseModel):
+    full_name: str
+    email: str
+    phone_number: str
+    address: str
+    box_count: int
+    comment: str
+
+
 @router.post("/")
-async def read_order(order: Request):
-    order = await order.json()
-    return await send_notification(order)
+async def read_order(order: Order):
+    data = await send_notification(order.dict())
+    return JSONResponse(content=data, status_code=data.get("status", 500))
