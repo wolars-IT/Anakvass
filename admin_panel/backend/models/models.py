8677225c3d25f313d97dbd269fd1b8482f777db2
Lo-Riot <lo_riot@riseup.net>
2023-07-15 14:54:40 +0300

    Add Admin model

--- admin_panel/backend/models/models.py
+++ admin_panel/backend/models/models.py
@@ -1,6 +1,25 @@
-from sqlalchemy.orm import DeclarativeBase
+import datetime
+
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import (
+    DeclarativeBase, Mapped, mapped_column
+)
 
 
 class Base(DeclarativeBase):  # Defines as a class for type checkers
     pass
 
+
+class Admin(Base):
+    __tablename__ = 'admins'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(25), nullable=False)
+    password: Mapped[str] = mapped_column(String(200), nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    last_login: Mapped[datetime.datetime] = mapped_column(
+        default=datetime.datetime.now(), nullable=False
+    )
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        default=datetime.datetime.now(), nullable=False
+    )
