4df3f9cb7ae9486138e4f706b591cab48699cf23
Lo-Riot <lo_riot@riseup.net>
2023-07-17 17:50:35 +0300

    Hash admin password

--- admin_panel/backend/db/repos.py
+++ admin_panel/backend/db/repos.py
@@ -1,6 +1,8 @@
+from passlib.context import CryptContext
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from models import models
-from schemas.admin import Admin, AdminCreate
+from schemas.admin import AdminCreate
 
 
 class BaseRepo:
@@ -10,7 +12,15 @@ class BaseRepo:
 
 class AdminRepo(BaseRepo):
     async def add(self, admin: AdminCreate) -> None:
-        self.session.add(models.Admin(**admin.model_dump()))
+        crypt_context = CryptContext(schemes=["argon2"])
+        hashed_password = crypt_context.hash(admin.password, scheme="argon2")
 
-    async def get(self, admin_id: int):
-        pass
+        self.session.add(models.Admin(
+            username=admin.username,
+            password=hashed_password,
+            last_login=admin.last_login,
+            created_at=admin.created_at
+        ))
+
+    async def get(self, admin_id: int) -> models.Admin | None:
+        return await self.session.get(models.Admin, admin_id)
