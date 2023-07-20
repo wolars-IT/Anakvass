0d52473f181e96a7e596f5d1bc111169a0f42252
Flop <uyweywgs@gmail.com>
2023-07-20 23:47:18 +0300

    Created Routes

--- customer_service/backend/routes/routes.py
+++ customer_service/backend/routes/routes.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter
-from schemas.order import Order
+from fastapi import Depends
+
 from typing_extensions import TypedDict
 
+from schemas.order import Order
+
+from services.customer import Customer
+from services.dependencies import CustomerService
 
 router = APIRouter()
 
 
 @router.post("/")
-async def create_order(order: Order) -> TypedDict("order_id", {"id": int}):
-    pass
+async def create_order(
+        order: Order,
+        customer: Customer = Depends(CustomerService(Customer))
+) -> TypedDict("order_id", {"id": int}):
+    order_id = await customer.send_order(order)
+    return {"id": order_id}
