b1a6d5648e109f4a7bb557db6c146f01b29fdde1
Lo-Riot <lo_riot@riseup.net>
2023-07-22 01:23:48 +0300

    Implement admin login and logout

--- admin_panel/backend/routes/auth.py
+++ admin_panel/backend/routes/auth.py
@@ -1,19 +1,23 @@
-from fastapi import APIRouter, Depends, HTTPException
+from os import getenv
+from fastapi import (
+    APIRouter, Depends, HTTPException,
+    Request, Response
+)
 
-from db.repos import AdminRepo
-from db.dependencies import Repo
 from schemas.admin import AdminSchema, AdminLoginSchema
-
 from services.auth import AuthService
-from services.dependencies import AdminService
+
+COOKIE_NAME = getenv("COOKIE_NAME", "sessionid")
+COOKIE_AGE = getenv("COOKIE_AGE", 2592000)
 
 router = APIRouter(prefix="/auth")
 
 
 @router.post("/login")
 async def login(
+    response: Response,
     admin_login_data: AdminLoginSchema,
-    auth_service: AuthService = Depends(AdminService(AuthService))
+    auth_service: AuthService = Depends(AuthService),
 ):
     admin = await auth_service.authenticate(admin_login_data)
     if admin is None:
@@ -21,12 +25,31 @@ async def login(
             status_code=403, detail="Wrong username or password"
         )
 
+    session_token = await auth_service.login(admin)
+    response.set_cookie(COOKIE_NAME, session_token, max_age=COOKIE_AGE)
+
 
 @router.post("/logout")
-async def logout():
-    pass
+async def logout(
+    request: Request,
+    response: Response,
+    auth_service: AuthService = Depends(AuthService)
+):
+    session_token = request.cookies.get(COOKIE_NAME)
+    await auth_service.logout(session_token)
+    response.delete_cookie(COOKIE_NAME)
 
 
 @router.get("/current_user")
-async def read_current_user(admin_repo: AdminRepo = Depends(Repo(AdminRepo))):
-    pass
+async def read_current_user(
+    request: Request,
+    auth_service: AuthService = Depends(AuthService)
+) -> AdminSchema:
+    session_token = request.cookies.get(COOKIE_NAME)
+    admin = await auth_service.get_current_admin(session_token)
+
+    if admin is None:
+        raise HTTPException(
+            status_code=403, detail="Access denied"
+        )
+    return admin
--- admin_panel/backend/services/auth.py
+++ admin_panel/backend/services/auth.py
@@ -1,19 +1,35 @@
+import secrets
+from os import getenv
+
+from redis.asyncio.client import Redis
 from fastapi import Depends
 
 from db.repos import AdminRepo
-from db.dependencies import Repo
 
 from schemas.admin import AdminLoginSchema
 from models.models import Admin
 
 from services.base import BaseAdminService
+from services.dependencies import get_redis
+
+
+COOKIE_AGE = getenv("COOKIE_AGE", 2592000)
 
 
 class AuthService(BaseAdminService):
+    def __init__(
+        self,
+        repo: AdminRepo = Depends(AdminRepo),
+        redis: Redis = Depends(get_redis)
+    ):
+        super().__init__(repo)
+        self.redis = redis
+        self.cookie_age = COOKIE_AGE
+
     async def authenticate(
         self, admin_login_data: AdminLoginSchema
     ) -> Admin | None:
-        """Verifies admin password, saves admin session in the database"""
+        """Verifies admin password"""
         admin = await self.repo.get_by_username(admin_login_data.username)
 
         if admin is None:
@@ -25,19 +41,27 @@ class AuthService(BaseAdminService):
 
         return admin
 
-    async def login(self):
-        """Sets session of the authenticated admin, adds it to the headers"""
+    async def login(self, admin: Admin) -> str:
+        """Saves session in the database, returns session token"""
+        session_token = secrets.token_urlsafe()
+        await self.redis.set(session_token, admin.id, ex=self.cookie_age)
+        return session_token
+
+    async def logout(self, session_token: str) -> None:
+        if session_token is not None:
+            await self.redis.delete(session_token)
 
     async def get_current_admin(
-        self, admin_repo: AdminRepo = Depends(Repo(AdminRepo))
-    ) -> Admin:
+        self, session_token: str | None
+    ) -> Admin | None:
         """Returns current authenticated admin.
         Can be used by middlewares and routes.
         """
-        session = self._get_current_session()
+        if session_token is None:
+            return None
 
-    async def _set_current_session(self):
-        pass
+        admin_id = await self.redis.get(session_token)
+        if admin_id is None:
+            return None
 
-    async def _get_current_session(self):
-        """Returns object that contains admin_id"""
+        return await self.repo.get(admin_id)
