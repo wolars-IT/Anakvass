28114a3e73483889900265dcca85bdc68c3b03e8
Tapio-adro <terentuk4321@gmail.com>
2023-07-22 13:03:26 +0200

    Added style.sass

--- customer_service/frontend/src/index.jsx
+++ customer_service/frontend/src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "./styles/stylereset.sass";
-import "./styles/variables.sass";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
