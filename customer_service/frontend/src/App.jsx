720c2cd0d9848fc38e1273c08bf90da9fe4374ba
Tapio-adro <terentuk4321@gmail.com>
2023-07-23 11:47:18 +0200

    Finished "story" section
    Also renamed some tags

--- customer_service/frontend/src/App.jsx
+++ customer_service/frontend/src/App.jsx
@@ -14,17 +14,17 @@ function App() {
   ];
   const navigationList = navigation.map((item) => {
     return (
-      <div className={"nav_item " + (item.isActive ? "active" : "")}>
+      <button className={"nav_item " + (item.isActive ? "active" : "")}>
         {item.name}
-      </div>
+      </button>
     )
   })
 
   return (
     <>
       <div id="language_switch">
-        <div className="switch_half left_half">UK</div>
-        <div className="switch_half right_half">UA</div>
+        <button className="switch_half left_half">UK</button>
+        <button className="switch_half right_half">UA</button>
       </div>
       <img src={spiraltop} id="spiral_top"/>
       <img src={spiralright} id="spiral_right"/>
@@ -33,25 +33,31 @@ function App() {
       <nav id="nav">
         {navigationList}
       </nav>
-      <div id="main">
+      <section id="main">
         <div id="bottle_wrapper">
           <img src={bottle} id="bottle_top"/>
         </div>
 
-        <div id="header">Анаквас</div>
-        <div id="header_caption">
+        <div id="main_header">Анаквас</div>
+        <div id="main_header_caption">
           Інновативний напій натурального бродіння зі смаком ананасу
         </div>
-        <div id="order">
-          <div className="order_wrapper">
-            <div className="order_button">Замовити</div>
+          <div id="order_button">
+            <div className="order_text">Замовити</div>
             <div className="order_arrow">
               <img src={arrowdown}/>
             </div>
-          </div>
         </div>
-      </div>
-
+      </section>
+      <section id="story">
+        <div className="header">
+          Історія
+          <img src={spiral} id="spiral"/>
+        </div>
+        <div id="story_header_caption">
+          Одного разу я випадково знайшов статтю про те, як можна навіть зі шкірок Ананасу зробити мега-корисний квасовий напій у домашніх умовах. Озброївшись гострим ножем, чистими пляшками, ананасом та ентузіазмом я взявся до роботи. І вже за 3 дні вийшов він, витвір мистецтва бродіння: Анаквас.
+        </div>
+      </section>
     </>
   );
 }
