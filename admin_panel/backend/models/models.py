52da60c02d4c9c6aade02847b6cbf53271346a2b
Lo-Riot <lo_riot@riseup.net>
2023-07-27 20:53:49 +0300

    Add order schema and model

--- admin_panel/backend/models/models.py
+++ admin_panel/backend/models/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from schemas.order import Statuses
 
-from sqlalchemy import String
+from sqlalchemy import String, Enum
 from sqlalchemy.orm import (
     DeclarativeBase, Mapped, mapped_column
 )
+from sqlalchemy.dialects.mysql import INTEGER
 
 
 class Base(DeclarativeBase):  # Defines as a class for type checkers
@@ -23,3 +25,16 @@ class Admin(Base):
     created_at: Mapped[datetime] = mapped_column(
         default=datetime.now(), nullable=False
     )
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id: Mapped[int]           = mapped_column(primary_key=True)
+    full_name: Mapped[str]    = mapped_column(String(125), nullable=False)
+    email: Mapped[str]        = mapped_column(String(320), nullable=False)
+    phone_number: Mapped[str] = mapped_column(String(15), nullable=False)
+    address: Mapped[str]      = mapped_column(String(425), nullable=False)
+    box_count: Mapped[str]    = mapped_column(INTEGER(unsigned=True), nullable=False)
+    comment: Mapped[str]      = mapped_column(String(255), nullable=True)
+    status: Mapped[Statuses]  = mapped_column(Enum(Statuses), nullable=False)
---
+++ admin_panel/backend/schemas/order.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from pydantic import BaseModel, constr, conint
+
+
+class Statuses(Enum):
+    New = "New"
+    Hot = "Hot"
+    Sending = "Sending"
+    Done = "Done"
+    Frozen = "Frozen"
+
+
+class OrderSchema(BaseModel):
+    id: int
+    full_name: constr(min_length=8, max_length=255)
+    email: constr(min_length=8, max_length=320)
+    phone_number: constr(min_length=6, max_length=15)
+    address: constr(min_length=12, max_length=425)
+    box_count: conint(gt=0, lt=(2**32))
+    comment: constr(min_length=1, max_length=255) | None
