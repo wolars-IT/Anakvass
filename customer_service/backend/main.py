82859e3f9d5dec0d49049156fb75f6d361f8fb4d
Flop <uyweywgs@gmail.com>
2023-08-02 15:32:51 +0300

    Add CORS & Middleware

--- customer_service/backend/main.py
+++ customer_service/backend/main.py
@@ -1,4 +1,8 @@
+from settings import settings
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes import routes
 
 
@@ -8,3 +12,16 @@ app = FastAPI(
 )
 
 app.include_router(routes.router)
+
+origins = [settings.url]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=[
+        "Content-Type",
+        "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+    ],
+)
--- customer_service/backend/settings.py
+++ customer_service/backend/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url: str
+    url: str = "http://localhost:8000"
 
 
 settings = Settings()
