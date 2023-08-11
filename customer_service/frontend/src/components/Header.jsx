0d4bb24fbf7c6a35ba31854cdf0ced24ad189610
Tapio-adro <terentuk4321@gmail.com>
2023-08-11 09:50:27 +0200

    Optimized language switch

--- customer_service/frontend/src/components/Header.jsx
+++ customer_service/frontend/src/components/Header.jsx
@@ -6,35 +6,35 @@ export default function Header() {
   const { t, i18n } = useTranslation();
 
   const [showMenu, setShowMenu] = useState(false);
-  const [lngs, setLngs] = useState([
-    {code: "uk", isActive: true},
-    {code: "en", isActive: false}
+  const [languages, setLanguages] = useState([
+    {code: "uk", isActive: true, text: "UK"},
+    {code: "en", isActive: false, text: "EN"}
   ]);
 
   useEffect(() => {
-    const localLanguage = window.localStorage.getItem('language')
+    const localLanguage = window.localStorage.getItem("language")
     if (localLanguage !== null) {
       changeLanguage(JSON.parse(localLanguage))
     }
   }, [])
 
-  const changeLanguage = (lngCode) => {
-    const nextLngs = lngs.map((lng) => {
-      if (lng.code == lngCode) {
+  const changeLanguage = (languageCode) => {
+    const nextLanguages = languages.map((language) => {
+      if (language.code == languageCode) {
         return {
-          ...lng,
+          ...language,
           isActive: true
         }
       }
       return {
-        ...lng,
+        ...language,
         isActive: false
       }
     })
 
-    setLngs(nextLngs)
-    i18n.changeLanguage(lngCode);
-    window.localStorage.setItem('language', JSON.stringify(lngCode));
+    setLanguages(nextLanguages)
+    i18n.changeLanguage(languageCode);
+    window.localStorage.setItem("language", JSON.stringify(languageCode));
   }
   const toggleMenu = () => {
     setShowMenu(prev => !prev);
@@ -59,18 +59,26 @@ export default function Header() {
       </a>
     </li>
   )
+  const languageButtons = languages.map((language) => {
+    const languageButtonClasses = classNames(
+      "language-switch__half", 
+      {"language-switch__half_active": language.isActive
+    });
+    
+    return (
+      <button
+        key={language.code}
+        className={languageButtonClasses} 
+        onClick={() => changeLanguage(language.code)}
+      >
+        {language.text}
+      </button>
+    )
+  })
 
   const navClasses = classNames("nav", {"nav_open": showMenu});
   const menuButtonClasses = classNames("menu-button", {"menu-button_active": showMenu});
-  const leftLngButtonClassNames = classNames(
-    "language-switch__half", 
-    "language-switch__left_half", 
-    {"language-switch__half_active": lngs[0].isActive});
-  const rightLngButtonClassNames = classNames(
-    "language-switch__half", 
-    "language-switch__right_half", 
-    {"language-switch__half_active": lngs[1].isActive});
-  
+
   return (
     <header className="header">
       <div className="container header__container">
@@ -82,7 +90,7 @@ export default function Header() {
 
         <div className={menuButtonClasses} onClick={toggleMenu}>
           <svg xmlns="http://www.w3.org/2000/svg" width="68" height="7" viewBox="0 0 68 7" fill="none">
-              <path d="M1 6C4.30116 6 4.30116 1 7.60232 1C10.9035 1 10.9035 6 14.2046 6C17.5058 6 17.5058 1 20.7992 1C24.1005 1 24.1005 6 27.3938 6C30.6951 6 30.6951 1 33.9884 1C37.2896 1 37.2896 6 40.5906 6C43.8918 6 43.8918 1 47.1931 1C50.4941 1 50.4941 6 53.7953 6C57.0965 6 57.0965 1 60.3978 1C63.6988 1 63.6988 6 67 6" stroke="#E0AB21" strokeWidth="1.5929" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M1 6C4.30116 6 4.30116 1 7.60232 1C10.9035 1 10.9035 6 14.2046 6C17.5058 6 17.5058 1 20.7992 1C24.1005 1 24.1005 6 27.3938 6C30.6951 6 30.6951 1 33.9884 1C37.2896 1 37.2896 6 40.5906 6C43.8918 6 43.8918 1 47.1931 1C50.4941 1 50.4941 6 53.7953 6C57.0965 6 57.0965 1 60.3978 1C63.6988 1 63.6988 6 67 6" stroke="#E0AB21" strokeWidth="1.5929" strokeMiterlimit="10" strokeLinecap="round"/>
           </svg>
           <svg xmlns="http://www.w3.org/2000/svg" width="68" height="7" viewBox="0 0 68 7" fill="none">
             <path d="M1 6C4.30116 6 4.30116 1 7.60232 1C10.9035 1 10.9035 6 14.2046 6C17.5058 6 17.5058 1 20.7992 1C24.1005 1 24.1005 6 27.3938 6C30.6951 6 30.6951 1 33.9884 1C37.2896 1 37.2896 6 40.5906 6C43.8918 6 43.8918 1 47.1931 1C50.4941 1 50.4941 6 53.7953 6C57.0965 6 57.0965 1 60.3978 1C63.6988 1 63.6988 6 67 6" stroke="#E0AB21" strokeWidth="1.5929" strokeMiterlimit="10" strokeLinecap="round"/>
@@ -94,8 +102,7 @@ export default function Header() {
 
       </div>
         <div className="language-switch">
-          <button className={leftLngButtonClassNames} onClick={() => changeLanguage("uk")}>UK</button>
-          <button className={rightLngButtonClassNames} onClick={() => changeLanguage("en")}>EN</button>
+          {languageButtons}
         </div>
     </header>
   )
